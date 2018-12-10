4a9420f927f9921826ae0f0531f1f56a942d63c8
1fengchen1 <758896823@qq.com>
2018-12-10 17:05:22 +0800

    '2c'

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_script import Manager
+
+
 app = Flask(__name__)
+# flask_script扩展包，命令行解释器
+manager = Manager(app)
 
 
 @app.route('/')
@@ -13,4 +18,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
