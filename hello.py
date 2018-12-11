416600b911c067e15315ff530b40df4d3df009db
1fengchen1 <758896823@qq.com>
2018-12-11 16:44:43 +0800

    表单使用“请求上下文的会话”session来解决post表单提交后刷新的问题

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
@@ -16,23 +16,22 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     # NameForm()实例表示表单
     form = NameForm()
     # validate_on_submit对表单数据进行验证(验证函数验证)
     if form.validate_on_submit():
-        # 将表单数据传给局部变量
-        name = form.name.data
-        # 清空表单数据
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+        # 使用请求上下文的会话功能，就会避免post表单提交，然后刷新造成的提示“提交表单”的问题
+        session['name'] = form.name.data
+        # 重定向==redirect('/');使用url_for的原因:使用URL映射生成URL,保证URL和定义的路由兼容，修改路由名字后依然可用
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
