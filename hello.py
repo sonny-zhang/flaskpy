b49d0cdff3a684331df3a1d35a6dc9a00fe22a8b
1fengchen1 <758896823@qq.com>
2018-12-11 16:09:42 +0800

    表单处理

--- hello.py
+++ hello.py
@@ -2,19 +2,37 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 # manager = Manager(app)
 moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    # NameForm()实例表示表单
+    form = NameForm()
+    # validate_on_submit对表单数据进行验证(验证函数验证)
+    if form.validate_on_submit():
+        # 将表单数据传给局部变量
+        name = form.name.data
+        # 清空表单数据
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
